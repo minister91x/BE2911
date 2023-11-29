16e0e17b1a7a85e65555826769d7e42fffbaa16d
quan91 <quan91.nt@gmail.com>
2023-11-29 22:03:12 +0700

    Ađ api

---
+++ BE_ASPNET_2911/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using BE_ASPNET_2911.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_ASPNET_2911.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpPost("GetData")]
+        public async Task<ActionResult> GetData(string name)
+        {
+            var list = new List<DemoResponseData>();
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    list.Add(new DemoResponseData { Name = "Khoa hoc BACKEND " + i + " " + name });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return Ok(list);
+        }
+    }
+}
---
+++ BE_ASPNET_2911/Models/DemoResponseData.cs
@@ -0,0 +1,7 @@
+﻿namespace BE_ASPNET_2911.Models
+{
+    public class DemoResponseData
+    {
+        public string Name { get; set; }
+    }
+}
